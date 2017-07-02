95b16383eebd36cdc28e2733de2258100017e41d
Vedarth Sharma <vedarth.sharma@gmail.com>
2017-07-02 05:44:38 +0530

    twitter bot pre alpha complete

---
+++ hidden.py
@@ -0,0 +1,4 @@
+# Get the consumer,token key and secret by creating an app at dev.twitter.com
+def oauth():
+    return {'consumer_key':'yourkey', 'consumer_secret':'yoursecret', 'token_key':'yourtokenkey','token_secret':'yourtokensecret'}
+# These codes will be unique and confidential. Don't share them
